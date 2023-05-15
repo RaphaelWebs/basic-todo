1576d1a876a15f974378b5446e13fed7aeeaae26
Raphael Duran <raphaelwebs@gmail.com>
2023-05-15 03:50:34 -0400

    Edited readme file and added db 💽

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-Chili
+# 📝 PHP To-Do App
+
+This repository contains a basic to-do app implemented in PHP to showcase my PHP and SQL knowledge. The to-do app allows users to create, view, and delete tasks, as well as mark them as completed ✅ . It provides a simple and intuitive user interface for managing tasks efficiently. I'm sorry you have to see another to-do app, you must be tired.
+
+## Features ✨
+
+- Task management: Users can create, view, and delete tasks. ✍️📖❌
+- Task status: Users can mark tasks as completed or incomplete. ✅🚫
+- Responsive design: The app is designed to be mobile-friendly and responsive across different devices. 📱💻
+- Database connected. All tasks get stored to the database for later viewing. 💾🕦
+
+## Technologies Used 🛠️
+
+- PHP: The back-end logic of the app is implemented in PHP, providing server-side processing and database interactions. 🐘
+- MySQL: The app uses MySQL database to store and retrieve task data. 🗄️
+- HTML/CSS: The front-end interface is built using HTML and CSS, providing a clean and user-friendly design. 🌐🎨
+- JavaScript: The app uses a little JavaScript to decrease the need so reloads. 📃🔄
+
+## Live Demo 🌐
+
+A live demo of the to-do app is available at [Demo Link](https://raphaelduran.com/todo/). ▶️🌐
---
+++ db/to_do_list.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 15, 2023 at 09:37 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `to_do_list`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todos`
+--
+
+CREATE TABLE `todos` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `date_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `checked` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `todos`
+--
+
+INSERT INTO `todos` (`id`, `title`, `date_time`, `checked`) VALUES
+(55, 'Send my github link to TNG', '2023-05-15 03:31:23', 1),
+(56, 'Sleep 😴', '2023-05-15 03:31:54', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `todos`
+--
+ALTER TABLE `todos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `todos`
+--
+ALTER TABLE `todos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
